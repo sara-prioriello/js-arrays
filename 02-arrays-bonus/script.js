c15b253f23258f4af882c66ec0c3650bb0a1ab8b
sara-prioriello <saraprioriello6@gmail.com>
2026-03-07 10:31:22 +0100

    add methods push and splice

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,39 +10,43 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-
+const reversedTeachers = [];
 for(let i=0; i<teachers.length; i++){
  
   //console.log(i);
   let reversed = teachers.length -1 -i;
   //console.log(reversed);
   //console.log(teachers[reversed]);
-  const reversedTeachers = teachers[reversed];
+
+  reversedTeachers.push(teachers[reversed]);
   //reversedTeachers == teachers[reversed];
-  console.log(reversedTeachers);
+  
 }
 
-
+console.log(reversedTeachers);
 
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-//let longNames = null;
+let longNames = [];
 for(let i=0; i<teachers.length; i++){
   
   if (teachers[i].length>=5){
     
-    const longNames = teachers[i];
-    console.log(longNames)
+    longNames.push(teachers[i]);
+    
   }
 }
+console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 for(let i=0; i<teachers.length; i++){
   
   if (teachers[i] == "Ed"){
     
-    delete teachers[i];
+    //delete teachers[i];
+    teachers.splice(i,1)
+   
     
   }
 
